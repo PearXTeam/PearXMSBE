ed7b1b55655e041e599a0c94c8cdc3e181978863
mrAppleXZ <mr.applexz@gmail.com>
2016-12-19 08:57:46 +0500

    Init

---
+++ FileUtils.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PearXMSBE
+{
+	public class FileUtils
+	{
+		public static string[] GetFilesInDir(string dir)
+		{
+			List<string> l = new List<string>();
+			GetFilesInDir(dir, l);
+			return l.ToArray();
+		}
+
+		static void GetFilesInDir(string dir, List<string> toAdd)
+		{
+			foreach (string d in Directory.GetDirectories(dir))
+			{
+				GetFilesInDir(d, toAdd);
+			}
+			foreach (string f in Directory.GetFiles(dir))
+			{
+				toAdd.Add(f);
+			}
+		}
+
+		public static string GetRelativeString(string full, string relative, bool ignoreCase, char? dirSepChar = null)
+		{
+			StringComparison com = ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
+			if (dirSepChar != null)
+			{
+				switch (dirSepChar.Value)
+				{
+					case '/':
+						full = full.Replace('\\', '/');
+						relative = relative.Replace('\\', '/');
+						break;
+					case '\\':
+						full = full.Replace('/', '\\');
+						relative = relative.Replace('/', '\\');
+						break;
+				}
+			}
+			if (full.StartsWith(relative, com))
+			{
+				return full.Substring(relative.Length);
+			}
+			return full;
+		}
+
+		public static string[] GetRelativeString(string[] full, string relative, bool ignoreCase, char? dirSepChar = null)
+		{
+			List<string> l = new List<string>();
+			foreach (string s in full)
+			{
+				l.Add(GetRelativeString(s, relative, ignoreCase, dirSepChar));
+			}
+			return l.ToArray();
+		}
+	}
+}
---
+++ PXCopy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.Build.Utilities;
+
+namespace PearXMSBE
+{
+	public class PXCopy : Task
+	{
+		public string StartMsg { get; set; } = "Copying files...";
+		public string StopMsg { get; set; } = "Files are copied successfully!";
+		public string From { get; set; }
+		public string To { get; set; }
+
+		public override bool Execute()
+		{
+			Log.LogMessage(StartMsg);
+			From = From.Replace('\\', Path.DirectorySeparatorChar);
+			To = To.Replace('\\', Path.DirectorySeparatorChar);
+			if (!From.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+				From = From + Path.DirectorySeparatorChar;
+			if (!To.EndsWith(Path.DirectorySeparatorChar.ToString(), StringComparison.Ordinal))
+				To = To + Path.DirectorySeparatorChar;
+			var fls = FileUtils.GetFilesInDir(From);
+			string[] relativePaths = FileUtils.GetRelativeString(fls, From, true);
+			for (int i = 0; i < fls.Length; i++)
+			{
+				string fFrom = fls[i];
+				string fTo = To + relativePaths[i];
+				Directory.CreateDirectory(Path.GetDirectoryName(fTo));
+				File.Copy(fFrom, fTo, true);
+			}
+			Log.LogMessage(StopMsg);
+			return true;
+		}
+	}
+}
